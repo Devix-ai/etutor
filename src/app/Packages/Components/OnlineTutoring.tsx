78fbe36411b2092e7c269245ff97b50c053ffca7
hashimnoor901 <hashimn901@gmail.com>
2024-08-26 10:34:36 +0500

    some changes in packages page

--- src/app/Packages/Components/OnlineTutoring.tsx
+++ src/app/Packages/Components/OnlineTutoring.tsx
@@ -7,15 +7,15 @@ import img3 from "../../../../public/assets/homepage/nohidden.png"
 import img4 from "../../../../public/assets/homepage/robust.png"
 const OnlineTutoring = () => {
   return (
-    <div className='w-full  py-40 mb:py-12 mb:w-11/12 mb:m-auto'>
+    <div className='w-full  py-40 mb:py-12 mb:w-11/12 mb:m-auto lg:py-24'>
         <div className='text-center'>
         <Headings className='text-center' heading='Secure Online Tutoring'/>
         <p className='text-2xl 2xl:text-4xl mt-5 text-[#534988] mb:text-base'>We provide a protected learning environment to foster student success.</p>
         </div>
-        <div className='relative w-[85%]  m-auto mt-16 mb:w-full '>
+        <div className='relative w-[90%]  m-auto mt-16 mb:w-full '>
   <div className='absolute inset-0 transform translate-x-5 translate-y-5 bg-[#CFCCDE] rounded-3xl shadow-lg'></div>
   <div className='relative bg-white rounded-3xl border-darkBlue border-2 px-8 py-4 mb:p-0'>
-    <div className='flex justify-around  items-center gap-10 p-6 mb:flex-col mb:gap-6'>
+    <div className='flex justify-around  items-center gap-10 p-6 mb:flex-col '>
     <div className='w-[25%]   flex flex-col gap-5 justify-center items-center mb:w-[70%]'>
         <Image className='' alt='' src={img1} />
         <h2 className='text-darkBlue lg:text-sm 2xl:text-[32px] leading-normal font-semibold text-center text-lg  mb:text-base' >Thoroughly screened and qualified eTutors</h2>
--- src/app/Packages/Components/PackageHero.tsx
+++ src/app/Packages/Components/PackageHero.tsx
@@ -5,7 +5,7 @@ const PackageHero = () => {
     <div className='flex flex-col justify-center items-center xl:py-44 lg:py-26
      py-48 mb:py-12 mb:text-center text-center'>
       <h2 className='text-darkBlue 2xl:text-[70px] text-5xl font-semibold mb:text-3xl'>Online Tutoring <span className='text-customBlue'>Packages</span> </h2>
-      <p className='text-[40px] font-medium w-[90%] text-darkBlue xl:text-[30px] mb:text-base mt-8 lg:text-2xl 2xl:text-4xl '>Our pricing is straightforward, flexible, and cost-effective for both group and individual sessions</p>
+      <p className='text-[42px] font-medium w-[90%] text-darkBlue xl:text-[30px] mb:text-base mt-8 lg:text-2xl 2xl:text-4xl '>Our pricing is straightforward, flexible, and cost-effective for both group and individual sessions</p>
     </div>
   )
 }
